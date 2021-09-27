0b50980ac33c3eecc67f518198665b07c6d827b9
obedpaz <lesterpaz99@gmail.com>
2021-09-26 19:36:07 -0600

    create a new intersection observer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import { removeImages } from './removeImg.mjs';
 
 toggleMessageValue();
 
-const fetchImageBtn = document.querySelector('#fetchImageBtn');
+const fetchImageBtn = document.querySelector('#fetch-image');
+const loadNewImageBtn = document.querySelector('#load-new-image');
+const buttons = document.querySelector('#buttons');
+
 fetchImageBtn.addEventListener('click', createImage);
+loadNewImageBtn.addEventListener('click', createImage);
 
 const cleanBtn = document.querySelector('#clean-btn');
 cleanBtn.addEventListener('click', removeImages);
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			buttons.classList.add('hidden');
+			return;
+		}
+		buttons.classList.remove('hidden');
+	});
+});
+
+const target = fetchImageBtn;
+observer.observe(target);
